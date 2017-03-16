601697dcf26b1e27cc3624b0a615ec0cf28a5ec3
Sam Chung <tieoneease@gmail.com>
2017-03-16 07:21:03 -0400

    lol

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 
-let API_URL = 'http://localhost:5000'
+let API_URL = 'https://ashchenapi.herokuapp.com/'
 
 let http = axios.create({ baseURL: API_URL })
 
