57256f530a1a1f3b9c847d57729fe512a65b9933
Sam Chung <tieoneease@gmail.com>
2017-03-16 07:30:25 -0400

    lol

--- dist/server.js
+++ dist/server.js
@@ -11,10 +11,18 @@ var port = process.env.PORT || 5000;
 
 //var fileURL = './data.json'
 //var cachedData = require('./data.json')
-
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
   next();
 });
 
